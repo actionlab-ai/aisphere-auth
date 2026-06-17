ecbb7704983dd4f6a976346e5e504456c8640047
元玉鹏 <51024420+yuanyp8@users.noreply.github.com>
2026-06-17 13:49:05 +0800

    feat: add public sdk sentinel errors

--- pkg/aisphereauth/errors.go
+++ pkg/aisphereauth/errors.go
@@ -3,7 +3,9 @@ package aisphereauth
 import "errors"
 
 var (
-	ErrUnauthorized = errors.New("unauthorized")
-	ErrForbidden    = errors.New("forbidden")
-	ErrNoPrincipal  = errors.New("principal not found")
+	ErrUnauthorized     = errors.New("unauthorized")
+	ErrForbidden        = errors.New("forbidden")
+	ErrNoPrincipal      = errors.New("principal not found")
+	ErrInactiveSession  = errors.New("inactive session")
+	ErrPermissionDenied = errors.New("permission denied")
 )
